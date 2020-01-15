eae0493221865ad89ae1ba228d85bc4050fd2387
Jody <jodyvermeulen007@gmail.com>
2020-01-15 08:52:03 +0200

    Add API

--- React/my-app/src/App.tsx
+++ React/my-app/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import "./components/api.js";
+
 const App: React.FC = () => {
   const [name, setName] = React.useState("");
   const [date, setDate] = React.useState("");
+  const [data, setTodo] = React.useState("");
   return (
     <div>
       <header className="App-header">
@@ -12,7 +15,12 @@ const App: React.FC = () => {
         </label>
       </header>
       <details className="details">
-        Name: <input type="text" onChange={e => setName(e.target.value)} />
+        Name:{" "}
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onChange={e => setName(e.target.value)}
+        />
         <button
           type="button"
           onClick={() => {
@@ -22,6 +30,23 @@ const App: React.FC = () => {
           className="button"
         >
           DateTime
+        </button>
+        <br></br>
+        TODO:{" "}
+        <input
+          type="text"
+          placeholder="Enter new Todo"
+          value={data}
+          onChange={e => setTodo(e.target.value)}
+        />
+        <button
+          type="submit"
+          onSubmit={() => {
+            setTodo(data);
+            todoApi.create(data.trimEnd());
+          }}
+        >
+          Add TODO
         </button>
       </details>
       <footer></footer>
