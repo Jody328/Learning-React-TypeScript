f9bd29e05a4040bad75d56d437523c157bce6e4e
Jody <jodyvermeulen007@gmail.com>
2020-01-17 15:26:21 +0200

    Add buttons to To-do list

--- React/my-app/src/App.tsx
+++ React/my-app/src/App.tsx
@@ -4,26 +4,35 @@ import { genericApi } from "./components/api";
 import { TodoList } from "./components/TodoList";
 import { NewTodo } from "./components/newTodo";
 
-type todoType = {
+export type todoType = {
   id: number;
   value: string;
+  isCompleted: boolean;
 };
-
 const genericTodos = genericApi<todoType>("todos");
 
 const App: React.FC = () => {
   const [name, setName] = React.useState("");
   const [date, setDate] = React.useState("");
+  const [search, setSearch] = React.useState("");
   const [todos, setTodos] = React.useState<todoType[]>([]);
 
   useEffect(() => {
     genericTodos.all().then(items => {
-      setTodos(items);
+      if (search) {
+        setTodos(
+          items.filter(item =>
+            item.value.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+          )
+        );
+      } else {
+        setTodos(items);
+      }
     });
-  }, []);
+  }, [search]);
 
   return (
-    <div>
+    <div className="app">
       <header className="App-header">
         <p>Welcome, {name}</p>
         <label className="Label">
@@ -48,6 +57,44 @@ const App: React.FC = () => {
           DateTime
         </button>
         <NewTodo />
+        <h1>TodoList</h1>
+        <div className="search">
+          <input
+            type="text"
+            name="searchValue"
+            placeholder="Search"
+            onChange={e => setSearch(e.target.value)}
+          />
+        </div>
+        <div className="sort">
+          <button
+            onClick={() => {
+              genericTodos.all().then(items => {
+                setTodos(items);
+              });
+            }}
+          >
+            ALL
+          </button>{" "}
+          <button
+            onClick={() => {
+              genericTodos.filterBy("isCompleted", false).then(items => {
+                setTodos(items);
+              });
+            }}
+          >
+            TO-DO
+          </button>{" "}
+          <button
+            onClick={() => {
+              genericTodos.filterBy("isCompleted", true).then(items => {
+                setTodos(items);
+              });
+            }}
+          >
+            DONE
+          </button>
+        </div>
         <TodoList todos={todos} />
       </div>
       <footer></footer>
